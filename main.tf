10b324707f746b181f42550136f4b98327255f42
Hee Won Kim <hkim@Hees-MacBook-Pro.local>
2019-05-05 22:44:02 +0900

    updated more ports

--- main.tf
+++ main.tf
@@ -114,6 +114,20 @@ resource "aws_security_group" "bastion" {
     cidr_blocks = ["121.137.63.104/32"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["121.137.63.104/32"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["121.137.63.104/32"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
